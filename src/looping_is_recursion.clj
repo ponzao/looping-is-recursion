0040c82cf6d7b0640fdf0e15f3a332997dddb575
Vesa Marttila <vtmarttila@gmail.com>
2013-10-27 16:12:53 +0200

    Implement functionality specified in tests

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,26 +1,71 @@
 (ns looping-is-recursion)
 
-(defn power [base exp]
-  ":(")
+(defn power
+  [base exp]
+  (loop [acc 1 i 0]
+    (if (>= i exp)
+      acc 
+      (recur (* acc base) (inc i)))))
 
-(defn last-element [a-seq]
-  ":(")
+(defn last-element
+  [a-seq]
+  (loop [acc nil coll a-seq]
+    (if (seq coll)
+      (recur (first coll) (rest coll))
+      acc)))
 
-(defn seq= [seq1 seq2]
-  ":(")
+(defn seq=
+  [seq1 seq2]
+  (loop [c1 seq1 c2 seq2]
+    (cond (and (empty? c1) (empty? c2)) true
+          (or (empty? c1) (empty? c2)) false
+          (= (first c1) (first c2)) (recur (rest c1) (rest c2))
+          :else false)))
 
-(defn find-first-index [pred a-seq]
-  ":(")
+(defn find-first-index
+  [pred a-seq]
+  (loop [i 0 coll a-seq]
+    (when-let [s (seq coll)]
+      (if (pred (first s))
+        i
+        (recur (inc i) (rest coll))))))
 
-(defn avg [a-seq]
-  -1)
+(defn avg
+  [a-seq]
+  (loop [coll a-seq sum 0 n 0]
+    (if (empty? coll)
+      (/ sum n)
+      (recur (rest coll)
+             (+ (first coll) sum)
+             (inc n)))))
 
-(defn parity [a-seq]
-  ":(")
+(defn- toggle
+  [a-set elem]
+  (if (a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
-(defn fast-fibo [n]
-  ":(")
+(defn parity
+  [a-seq]
+  (loop [acc #{} coll a-seq]
+    (if (empty? coll)
+      acc
+      (recur (toggle acc (first coll)) (rest coll)))))
 
-(defn cut-at-repetition [a-seq]
-  [":("])
+(defn fast-fibo
+  [n]
+  (loop [i 0 x 0 y 1]
+    (if (< i n)
+      (recur (inc i) (+ x y) x)
+      x)))
+
+(defn cut-at-repetition
+  [a-seq]
+  (loop [acc [] coll a-seq seen #{}]
+    (cond (empty? coll) acc
+          (seen (first coll)) acc
+          :else (recur (conj acc (first coll))
+                       (rest coll)
+                       (conj seen (first coll))))))
+      
 
